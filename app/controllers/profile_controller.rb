96867e44593831babee8f30980e68900f8cd5cdf
RJ Dabbar <rjdabbar@gmail.com>
2013-12-11 18:30:05 -0500

    friends don't duplicate

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,7 +7,7 @@ class ProfileController < ApplicationController
   def show
     user = User.find(params[:id]) 
     @profile = Profile.find_by_steam_id(user.uid) || Profile.create_for(user)
-    @friend_list = Friend.create_friend_list_for(user)
+    @friend_list = Friend.find_by_user_id(user.id) || Friend.create_friend_list_for(user)
   end
 
 
